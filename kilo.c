97f87c741ad44b644d705c2b74c11e0cade29d3a
mori5321 <moriwm77@gmail.com>
2021-10-07 00:04:01 +0900

    HOME END DELETE key

--- kilo.c
+++ kilo.c
@@ -29,6 +29,9 @@ enum editorKey {
     ARROW_DOWN,
     PAGE_UP,
     PAGE_DOWN,
+    HOME_KEY,
+    END_KEY,
+    DEL_KEY,
 };
 
 /*** data ***/
@@ -206,6 +209,13 @@ int editorReadKey() {
     while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
         if (nread == -1 && errno != EAGAIN) die("read");
     }
+
+    // For Debug
+    // if (iscntrl(c)) {
+    //     printf("%d\r\n", c);
+    // } else {
+    //     printf("%d, %c\r\n", c, c);
+    // }
     
     if (c == '\x1b') {
         char seq[3];
@@ -218,8 +228,13 @@ int editorReadKey() {
                 if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
                 if (seq[2] == '~') {
                     switch (seq[1]) {
+                        case '1': return HOME_KEY;
+                        case '3': return DEL_KEY;
+                        case '4': return END_KEY;
                         case '5': return PAGE_UP;
                         case '6': return PAGE_DOWN;
+                        case '7': return HOME_KEY;
+                        case '8': return END_KEY;
                     }
                 }
             } else {
@@ -228,7 +243,14 @@ int editorReadKey() {
                     case 'B': return ARROW_DOWN;
                     case 'C': return ARROW_RIGHT;
                     case 'D': return ARROW_LEFT;
+                    case 'H': return HOME_KEY;
+                    case 'F': return END_KEY;
                 }
+            }
+        } else if (seq[0] == 'O') {
+            switch (seq[1]) {
+                case 'H': return HOME_KEY;
+                case 'F': return END_KEY;
             }
         }
 
@@ -247,6 +269,12 @@ void editorProcessKeyPress() {
             write(STDOUT_FILENO, "\x1b[2J", 4);
             write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
+            break;
+        case HOME_KEY:
+            E.cx = 0;
+            break;
+        case END_KEY:
+            E.cx = E.screencols -1;
             break;
         case PAGE_UP:
         case PAGE_DOWN:
@@ -277,7 +305,7 @@ int main() {
     initEditor();
 
     while (1) {
-        editorRefreshScreen();
+        editorRefreshScreen(); // NOTE: when you debug key input, comment out this line.
         editorProcessKeyPress();
     }
 
