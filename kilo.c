cee3d180e9f286c3bb3712641005b0aefb9eba9d
mori5321 <moriwm77@gmail.com>
2021-10-06 09:12:06 +0900

    style

--- kilo.c
+++ kilo.c
@@ -146,15 +146,6 @@ int main() {
     while (1) {
         editorRefreshScreen();
         editorProcessKeyPress();
-        // char c = '\0';
-        // if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-        // if (iscntrl(c)) {
-        //     printf("%d\r\n", c);
-        // } else {
-        //     printf("%d ('%c')\r\n", c, c);
-        // }
-
-        // if (c == CTRL_KEY('q')) break;
     }
 
     return 0;
